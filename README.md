73468d43ee15dc18c92f50562ce8dda7373c45f0
mutesasira Denis <denisbrian07@gmail.com>
2021-12-24 17:12:44 +0200

    Completed day10

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# my_advent2020
- 
+<h1>Solving Advent Of Code For 2020</h1>
+ <p>I Will Solve Advent of code for year 2020</p>
+<p>For Some of the difficult problem I 
+used the some of  joe grue's code</p>
--- code/day10.py
+++ code/day10.py
@@ -7,16 +7,17 @@
 from typing import List
 from collections import Counter
 
+
 class Adapter:
     def __init__(self, output: str) -> None:
         self.output = output
-    
-    def parse(self) -> List[str]:
+
+    def parse(self) -> list[int]:
         return [
             int(out)
             for out in self.output.split("\n")
         ]
-    
+
     def jolt(self) -> Counter:
 
         # List of output jolts: output_jolts
@@ -25,10 +26,9 @@ class Adapter:
         output_jolts.append(max(output_jolts) + 3)
         output_jolts.insert(0, 0)
 
-
         # Sorting the output jolt to descending order
         output_jolts.sort(reverse=True)
-        
+
         # Initial the index and jolt_length
         index = 0
         next_index = 1
@@ -53,10 +53,34 @@ class Adapter:
         three_rating_jolt = jolt_values[1]
         return one_rating_jolt * three_rating_jolt
 
+    def count_paths(self) -> int:
+        adapters = self.parse()
+        adapters.append(0)
+        adapters.append(max(adapters) + 3)
 
-##
-### Testing 
-##
+        output = adapters[-1]
+        # Num way[i] is the number pf ways to get to i
+        num_ways = [0] * (output + 1)
+        num_ways[0] = 1
+
+        if 1 in adapters:
+            num_ways[1] = 1
+        if 2 in adapters and 1 in adapters:
+            num_ways[2] = 2
+        elif 2 in adapters:
+            num_ways[2] = 1
+
+        for n in range(3, output + 1):
+            if n not in adapters:
+                continue
+            num_ways[n] = num_ways[n - 3] + num_ways[n - 2] + num_ways[n - 1]
+
+        return num_ways[output]
+
+
+#
+# ** Testing **
+#
 TESTCASE = """28
 33
 18
@@ -91,14 +115,15 @@ TESTCASE = """28
 
 # Initialize the Adapter class: adapter
 adapter = Adapter(TESTCASE)
-assert type(adapter.parse()) == list  
-
+assert type(adapter.parse()) == list
+assert adapter.count_paths() == 19208
 
 ##
-### Submission process
+# ** Submission process **
 ##
 
 with open("/Volumes/LocalDiskB/Py/my_advent2020/inputs/day10_input.txt") as f:
-    # Initialize the Adapter class: subm_adapter
-    subm_adapter = Adapter(f.read())
-    print(subm_adapter.multipier())
+    # Initialize the Adapter class: submission_adapter
+    submission_adapter = Adapter(f.read())
+    print(submission_adapter.multipier())
+    print(submission_adapter.count_paths())
